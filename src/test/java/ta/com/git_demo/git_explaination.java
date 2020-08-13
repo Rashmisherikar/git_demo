c32530859ad0c456b947b012e96ac2f4a2c64759
Rashmisherikar <rashmishyam30@gmail.com>
2020-08-13 15:47:52 -0700

    first commit of my code

---
+++ src/test/java/ta/com/git_demo/git_explaination.java
@@ -0,0 +1,17 @@
+package ta.com.git_demo;
+
+public class git_explaination {
+
+	public static void main(String[] args) {
+		/*
+		 1. What is repository : place where we can store the project or code
+		  
+		 2.	What is Git : Version control system for tracking changes in source code during software development.
+		 
+		 Working on same project: 3 person, everyone of us will be keep changing the code.
+		 In a project we can have more than 1 people code can be merged and the same can be released.
+		 */
+
+	}
+
+}
