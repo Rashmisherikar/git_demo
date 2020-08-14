dbe199772dd22b0fec8395bca4761c70387fc3b0
Rashmisherikar <rashmishyam30@gmail.com>
2020-08-13 19:23:57 -0700

    trail commit

--- src/test/java/ta/com/git_demo/git_explaination.java
+++ src/test/java/ta/com/git_demo/git_explaination.java
@@ -10,8 +10,26 @@ public class git_explaination {
 		 
 		 Working on same project: 3 person, everyone of us will be keep changing the code.
 		 In a project we can have more than 1 people code can be merged and the same can be released.
+		
 		 */
 
+		/*
+		command prompt : 
+git --version  -->to check whether git installed
+cd D: -->to change from c drive to D drive where i have my workspace
+D:\workspace\git_demo
+dir
+git init
+git status ----> shows the files in red color means it is not added to git
+git add .classpath ---> it will add to the git
+git status --->changes to be commited i.e .classpath got added
+git add * --> * will add all the files under git_demo maven project
+git status ---> changes to be commited all files got added (green in color)
+git commit -m "first commit of my code"
+git push origin master ----> if not working then we will force fully push
+git push -f origin master
+git status
+		*/
 	}
 
 }
